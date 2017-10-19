db7a7eefef08cf8222226cc2f1638b680b7f5166
lbias <306508354@qq.com>
2017-10-19 23:42:44 +0800

    setup project

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_restplus import Resource, Api
+
+application = Flask(__name__)
+api = Api(application, 
+          version='0.1', 
+          title='Our sample API',
+          description='This is our sample API',
+)
+
+@api.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+if __name__ == '__main__':
+    application.run(debug=True)
