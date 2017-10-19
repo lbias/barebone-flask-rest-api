90d72a96233fc288a43b45655c7741780826e442
lbias <306508354@qq.com>
2017-10-20 00:18:28 +0800

    database integration

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
-from flask_restplus import Resource, Api
+from flask_restplus import Resource, Api, fields
+from database import db_session
+from models import BlogPost
 
 application = Flask(__name__)
 api = Api(application, 
@@ -12,6 +14,21 @@ api = Api(application,
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
+
+@api.route('/blog_posts')
+class BlogPosts(Resource):
+    model = api.model('Model', {
+        'id': fields.Integer,
+        'title': fields.String,
+        'post': fields.String,
+    })
+    @api.marshal_with(model, envelope='resource')
+    def get(self, **kwargs):
+        return BlogPost.query.all()
+
+@application.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
 
 if __name__ == '__main__':
     application.run(debug=True)
